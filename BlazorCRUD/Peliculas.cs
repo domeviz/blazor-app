3d9e5cc2e9818ca90cf35b7382d7322d5c718341
Domenica Vizcarra <domeviz3410@gmail.com>
2025-04-15 07:48:54 -0500

    Conexion a SQL

---
+++ BlazorCRUD/Peliculas.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCRUD
+{
+    public class Peliculas
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+}
+}
